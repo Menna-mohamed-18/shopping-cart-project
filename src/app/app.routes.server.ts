ce02a42ed8ed82fee491746383e4af0814581a06
Menna-mohamed-18 <mennaaboelella18@gmail.com>
2025-03-01 19:53:43 +0200

    test2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+
+
+  {
+    path:'checkout/:id',
+    renderMode:RenderMode.Server
+  },
+  {
+    path:'details/:id',
+    renderMode:RenderMode.Server
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
